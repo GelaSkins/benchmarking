5d6b6c7487c60ba3845c1df60fa68dca00dd2086
Leandro Santos <leandro@gelaskins.com>
2014-04-30 12:48:00 -0400

    Refactorings

--- benchmarking/wrap_all_devices.rb
+++ benchmarking/wrap_all_devices.rb
@@ -3,35 +3,55 @@ require_relative '../timer'
 require 'csv'
 
 class WrapAllDevices
+  attr_reader :wrap_size, :artwork
+
   def run(wrap_size, artwork_id, csv_path)
-    wrap_size = wrap_size || 'normal'
-    csv_path  = csv_path  || Dir.pwd
+    @wrap_size = wrap_size || 'normal'
+    @artwork = find_artwork artwork_id
+    csv_path  = csv_path || Dir.pwd
     times = []
-    art = artwork(artwork_id)
+
     devices.each do |device|
-      resp = Timer.time { for_artwork(device, art, wrap_size) }
+      resp = Timer.time { curl_wrap device }
       time = Timer.elapsedTime
-      times << [device, time, art, resp]
+      times << [device, time, artwork, resp]
     end
 
     CSV.open(File.join(csv_path, 'server_wrap_times.csv'), 'wb') do |csv|
       csv << ['Device id','Device name','Time elapsed','Artwork', 'Http response']
       times.each do |row|
-        csv << %W(#{row.first.device_id} #{row.first.name} #{row[1]} #{row[2].name}, #{row.last.code})
+        csv << %W(#{row.first.device_id}
+                   #{row.first.name}
+                   #{row[1]}
+                   #{row[2].name}
+                   #{row.last.code}
+                )
       end
     end
   end
 
-  def for_artwork(device, artwork, size)
-    uri = URI(URI.escape("#{Settings.image_server_url}/public_wraps/iphone/#{artwork.artist_name}/#{artwork.artwork_id}/#{device.device_id}/#{size}/benchmark.jpg"))
+  def curl_wrap(device)
+    uri = URI(URI.escape(make_server_url(device)))
     Net::HTTP.get_response uri
+  end
+
+  def make_server_url(device)
+    %W(#{Settings.image_server_url}
+        public_wraps
+        iphone
+        #{artwork.artist_name}
+        #{artwork.artwork_id}
+        #{device.device_id}
+        #{wrap_size}
+        benchmark.jpg
+      ).join('/')
   end
 
   def devices
     Device.all
   end
 
-  def artwork(id)
+  def find_artwork(id)
     if id
       Artwork.find_by_artwork_id id
     else
